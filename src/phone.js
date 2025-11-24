70575eda18704335682ef56b080eb7a717687347
William <william.jiang1101@gmail.com>
2025-11-24 12:54:16 -0500

    Fixed lint

--- src/phone.js
+++ src/phone.js
@@ -1,87 +1,55 @@
 class Phone {
-    batteryLevel = 100;
-    #password;
-    constructor(brand, model, password){
-        this.brand = brand;
-        this.model = model;
-        this.#password = password;
-    }
-    
-    get battery(){
-        return this.batteryLevel;
-    }
+  batteryLevel = 100;
 
-    makeCall(number){
-        this.batteryLevel -= 5;
-        return `Calling ${number}`;
-    }
+  #password;
 
-    charge(){
-        this.batteryLevel = 100;
-        return `Phone fully charged`
-    }
+  constructor(brand, model, password) {
+    this.brand = brand;
+    this.model = model;
+    this.#password = password;
+  }
 
-    unlock(password){
-        if(this.#password === password){
-            console.log(`Phone unlocked`);
-            return true;
-        }
-        console.log(`Invalid Password`)
-        return false;
+  get battery() {
+    return this.batteryLevel;
+  }
+
+  makeCall(number) {
+    this.batteryLevel -= 5;
+    return `Calling ${number}`;
+  }
+
+  charge() {
+    this.batteryLevel = 100;
+    return `Phone fully charged`;
+  }
+
+  unlock(password) {
+    if (this.#password === password) {
+      console.log(`Phone unlocked`);
+      return true;
     }
+    console.log(`Invalid Password`);
+    return false;
+  }
 }
 
-class iPhone extends Phone{
-    constructor(model, password, numberOfCameras){
-        super(`Apple`, model, password);
-        this.numberOfCameras = numberOfCameras;
-    }
+class iPhone extends Phone {
+  constructor(model, password, numberOfCameras) {
+    super(`Apple`, model, password);
+    this.numberOfCameras = numberOfCameras;
+  }
 
-    makeCall(number){
-        return `${super.makeCall(number)} using FaceTime audio`;
-    }
+  makeCall(number) {
+    return `${super.makeCall(number)} using FaceTime audio`;
+  }
 
-    faceTime(name) {
-        this.batteryLevel -= 10;
-        return `Facetiming ${name}`
-    }
+  faceTime(name) {
+    this.batteryLevel -= 10;
+    return `Facetiming ${name}`;
+  }
 }
 
 // TEST YOUR CODE HERE
 
-const flipPhone = new Phone("Nokia", "Flip", "TimeToLearn882");
-console.log(flipPhone); // Phone { brand: "Nokia", model: "Flip", batteryLevel: 100 }
-
-// 1. Making a call and checking battery
-console.log(flipPhone.makeCall("123-456-7890")); // Calling 123-456-7890
-console.log(flipPhone.battery); // 95
-
-// 2. Charging and checking battery
-console.log(flipPhone.charge("123-456-7890")); // Phone fully charged
-console.log(flipPhone.battery); // 100
-
-// 3. Unlocking the phone
-console.log(flipPhone.unlock("TimeToLearn882")); // Phone unlocked
-console.log(flipPhone.unlock("TimeToLearn88")); // Invalid Password
-
-const smartPhone = new iPhone("iPhone 14 Pro", "TimeToLearn882", 3);
-
-console.log(smartPhone); // iPhone { brand: "Apple", model: "iPhone 14 Pro", numberOfCameras: 3, batteryLevel: 100}
-
-// 1. Making a call 
-console.log(smartPhone.makeCall("555-1234")); // Calling 555-1234 using FaceTime audio
-console.log(smartPhone.batteryLevel); // Should be 95
-
-// 2. Using the faceTime method
-console.log(smartPhone.faceTime("Alice")); // Facetiming Alice
-console.log(smartPhone.batteryLevel); // Should be 85
-
-// 3. Charge the phone
-console.log(smartPhone.charge()); // Phone fully charged
-console.log(smartPhone.batteryLevel); // Should be 100
-
-// 4. Unlock the phone
-console.log(smartPhone.unlock("0000")); // Invalid password
-console.log(smartPhone.unlock("TimeToLearn882")); // true
 // DO NOT REMOVE
 module.exports = { Phone, iPhone };
--- src/pokemon.js
+++ src/pokemon.js
@@ -1,87 +1,55 @@
 class Pokemon {
-    #health = 100;
-    #level = 1;
-    static allPokemon = [];
-    constructor(name, type){
-        this.name = name;
-        this.type = type;
-        Pokemon.allPokemon.push(this);
-    }
+  #health = 100;
 
-    get health(){
-        return this.#health;
-    }
+  #level = 1;
 
-    get level(){
-        return this.#level;
-    }
+  static allPokemon = [];
 
-    levelUp(){
-        this.#level++;
-        this.#health += 10;
-        if(this.#health >= 100){
-            this.#health = 100;
-        }
-        console.log(`${this.name} leveled up to level ${this.#level}!`);
-    }
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+    Pokemon.allPokemon.push(this);
+  }
 
-    isFainted(){
-        if(this.#health <= 0){
-            return true;
-        }
-        return false;
-    }
+  get health() {
+    return this.#health;
+  }
 
-    attack(targetPokemon){
-        targetPokemon.#health -= (10 * targetPokemon.#level);
-        console.log(`${this.name} attacked ${targetPokemon.name}!`)
-    }
+  get level() {
+    return this.#level;
+  }
 
-    static getTotalPokemon(){
-        return Pokemon.allPokemon.length;
+  levelUp() {
+    this.#level++;
+    this.#health += 10;
+    if (this.#health >= 100) {
+      this.#health = 100;
     }
+    console.log(`${this.name} leveled up to level ${this.#level}!`);
+  }
 
-    static findByName(name){
-        return Pokemon.allPokemon.find(pokemon => pokemon.name === name)
+  isFainted() {
+    if (this.#health <= 0) {
+      return true;
     }
+    return false;
+  }
+
+  attack(targetPokemon) {
+    targetPokemon.#health -= 10 * targetPokemon.#level;
+    console.log(`${this.name} attacked ${targetPokemon.name}!`);
+  }
+
+  static getTotalPokemon() {
+    return Pokemon.allPokemon.length;
+  }
+
+  static findByName(name) {
+    return Pokemon.allPokemon.find((pokemon) => pokemon.name === name);
+  }
 }
 
 // TEST YOUR CODE HERE
-const charizard = new Pokemon("Charizard", "Fire");
-const squirtle = new Pokemon("Squirtle", "Water");
-
-// 1. Checking instance properties
-console.log(charizard); // Pokemon { name: "Charizard", type: "Fire" }
-console.log(squirtle); // Pokemon { name: "Squirtle", type: "Water" }
-
-// 2. Leveling up a Pokemon
-squirtle.levelUp(); // Squirtle leveled up to level 2!
-squirtle.levelUp(); // Squirtle leveled up to level 3!
-squirtle.levelUp(); // Squirtle leveled up to level 4!
-
-// 3. Checking the level
-// OR if you use the get syntax
-console.log(squirtle.level); // 2
-
-
-// 4. Checking on health
-// OR if you use the get syntax
-console.log(squirtle.health);   // Should be 100
-
-
-// 5. Attacking until one faints
-while (!charizard.isFainted()) {
-  squirtle.attack(charizard);  
-}
-console.log("Squirtle has fainted!");
-
-
-// 6. Finding a Pokemon instance
-console.log(Pokemon.findByName("Charizard")); // Pokemon { name: "Charizard", type: "Fire" }
-
-
-// 7. Viewing count of all Pokemon
-console.log("Total Pokemon:", Pokemon.getTotalPokemon()); // 2
 
 // DO NOT REMOVE
 module.exports = { Pokemon };
